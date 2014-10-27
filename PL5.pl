b1b334074bf4efee4e02cb15dfb23eb6d11329eb
António Silva <antoniosilva1017@gmail.com>
2014-10-27 15:18:34 +0000

    Added exercise 6

--- PL5.pl
+++ PL5.pl
@@ -11,7 +11,7 @@ elimina_todos(V,[V|T],LR):- elimina_todos(V,T,LR), !.
 elimina_todos(V,[H|T], [H|LR]):- elimina_todos(V,T,LR).
 
 %	Exercício 2
-p([],[]). 
+p([],[]).
 p(L, [X|L1]):- elimina1(X,L,Li), p(Li,L1).
 
 %	Exercício 3
@@ -22,9 +22,38 @@ substitui(S,V,[H|T],[H|L]):- substitui(S,V,T,L).
 %	Exercício 4
 insere(V,P,L,LR):- insere(V,P,L,LR,1).
 insere(V,P,L,[V|L],P):-!.
-insere(V,P,[H|T],[H|LR],C):- C1 is C + 1, insere(V,P,T,LR,C1). 
+insere(V,P,[H|T],[H|LR],C):- C1 is C + 1, insere(V,P,T,LR,C1).
 
 %	Exercício 5
 inverte(L,LI):- inverte1(L,[ ],LI).
 inverte1([ ],L,L).
 inverte1([X|L],LI,LF):- inverte1(L,[X|LI],LF).
+
+%	Exercício 6
+
+união([ ],L,L).
+união([X|L],L1,L2):- member(X,L1),!,união(L,L1,L2). 
+união([X|L],L1,[X|L2]):- união(L,L1,L2).
+
+%	c)
+intersecção([],L2,[]).
+intersecção([H|L1],L2,[H|LR]):- member(H,L2), !,intersecção(L1,L2,LR).
+intersecção([H|L1],L2,LR):- intersecção(L1,L2,LR).
+
+%	d)
+diferenca_conjuntos(L1,L2,LR):- união(L1,L2,LU), intersecção(L1,L2,LI), elimina_lista(LU,LI,LR).
+
+elimina_lista([],L2,[]).
+elimina_lista([H|L1],L2,LR):- member(H,L2), !, elimina_lista(L1,L2,LR).
+elimina_lista([H|L1],L2,[H|LR]):- elimina_lista(L1,L2,LR).
+
+
+
+
+
+
+
+
+
+
+
