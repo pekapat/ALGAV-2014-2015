b312e9667e7ec3e6fcb5435823a66b53fa9e832a
António Silva <antoniosilva1017@gmail.com>
2014-11-04 19:22:14 +0000

    Solved exercise 2c

--- PL6.pl
+++ PL6.pl
@@ -45,3 +45,53 @@ caminho(O,D,LA,[O|L]):- liga(O,X), \+member(X,LA), caminho(X,D,[X|LA],L).
 %	inverte(L,LI) :- inverte(L,[],LI).
 %	inverte([],LI,LI).
 %	inverte([H|T],L,LI) :- inverte(T,[H|L],LI).
+
+
+%	Exercício 1.b)
+
+todos_caminhos(O,D,L):- findall(P,caminho(O,D,P),L).
+
+%	Exercício 1.c)
+
+menor_caminho(O,D,L):- todos_caminhos(O,D,C), menor(C,L).
+
+menor([H|[]],H):-!.
+menor([H|T],M):- menor(T,L1), (length(L1,M1), length(H,M2), M1 < M2, M = L1; M = H).
+
+%	Exercício 2.a)
+
+%	Base de conhecimento
+
+linha(1,[a,b,c,d,e,f]).
+linha(2,[g,b,h,i,j,k]).
+linha(3,[l,j,m,n,o,d,p]).
+linha(4,[f,q,r,s,t]).
+linha(5,[t,u,j,v,a,g]).
+
+gera_cruzamentos:- findall(_,cruzamento,_).
+
+cruzamento:- linha(N1,LE1), linha(N2,LE2), N1 \== N2, intersection(LE1,LE2,LI), LI \== [], assertz(cruza(N1,N2,LI)).
+
+%	Exercício 2.b)
+
+gera_estações:- findall(L, linha(_,L), LE), gera_estações(LE,Estacoes), assertz(estações(Estacoes)).
+
+gera_estações([], []).
+gera_estações([H|T], LE):- gera_estações(T,LR), union(LR, H, LE).
+
+gera_estação_linhas:- estações(LE), gera_estação_linhas(LE).
+
+%	Exercício 2.c)
+
+gera_estação_linhas([]).
+gera_estação_linhas([H|T]):- findall(L, (linha(L,LE), member(H, LE) ), LN), assertz(estação_linha(H,LN)), gera_estação_linhas(T).
+
+
+
+
+
+
+
+
+
+
