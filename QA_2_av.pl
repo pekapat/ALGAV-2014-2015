2a5e4fcd31b8b2b7a0df45e14203118baf936fdd
António Silva <antoniosilva1017@gmail.com>
2014-11-11 11:17:55 +0000

    Added the PL QA_2

---
+++ QA_2_av.pl
@@ -0,0 +1,7 @@
+p3([],[]):- fail.
+p3([H|T],H):- member(H,T), !.
+p3([_|T],R):- p3(T,R).
+
+lstpares(LP,LR):- lstpares(LP,LA,LR1), union(LR1,LA,LR).
+lstpares([],[],[]).
+lstpares([(H,H1)|T], [H1|LA], [H|LR]):- lstpares(T,LA,LR).
